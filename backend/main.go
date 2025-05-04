93d41a85e788168abdeee6649500e6f873df17c5
kanishk-8 <kanishkkumar222004@gmail.com>
2025-05-04 23:18:07 +0530

    small fix

--- backend/main.go
+++ backend/main.go
@@ -19,7 +19,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173,https://procode-2xh5.onrender.com",
+		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173,https://procode-2xh5.onrender.com,https://procode-alpha.vercel.app",
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
